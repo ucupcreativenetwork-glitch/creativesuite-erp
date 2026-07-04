ba2d325a5c3987b8d605f13d3386d9017564d4fa
ucupcreativenetwork <ucupcreativenetwork@gmail.com>
2026-07-04 12:06:11 +0700

    feat: add missing ERP files (171/256)

---
+++ backend/config/hashing.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'driver' => env('HASH_DRIVER', 'argon2id'),
+
+    'bcrypt' => [
+        'rounds' => env('BCRYPT_ROUNDS', 12),
+        'verify' => env('HASH_VERIFY', true),
+    ],
+
+    'argon' => [
+        'memory' => env('ARGON_MEMORY', 65536),
+        'threads' => env('ARGON_THREADS', 1),
+        'time' => env('ARGON_TIME', 4),
+        'verify' => env('HASH_VERIFY', true),
+    ],
+
+    'rehash_on_login' => env('HASH_REHASH_ON_LOGIN', true),
+];
